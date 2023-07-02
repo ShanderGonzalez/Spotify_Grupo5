b1371930fc60c6e99fe42485cbf0dfe435e1aabd
Shander <kevin.gonzalez@epn.edu.ec>
2023-07-02 16:27:40 -0500

    Proyecto de Spotify Grupo 5

--- src/NSpotifyChino/CompartirMusica.java
+++ src/NSpotifyChino/CompartirMusica.java
@@ -10,7 +10,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.SocketException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
